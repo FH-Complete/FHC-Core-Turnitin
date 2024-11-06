45e6f74aa456c394c2a3dcda31bb2be21c4b83cd
Paolo <bison@technikum-wien.at>
2024-11-06 21:03:34 +0100

    Changed configs

--- config/APIClient.php.ex
+++ config/APIClient.php.ex
@@ -8,8 +8,8 @@ $config['api_connections'] = array(
 		'protocol' => 'https', // ssl by default... better!
 		'host' => 'api.technikum-wien.at', // server name
 		'path' => 'v1', // usually this is the path for the API
-		'authorization' => 'secret0', // authorization header value
-                'integration_name' => 'fhcdev',
+		'authorization' => 'secret', // authorization header value
+                'integration_name' => 'FHC',
                 'integration_version' => 'v1'
 	)
 );
---
+++ config/global.php.ex
@@ -0,0 +1,11 @@
+<?php
+
+// Owner used to submit submissions
+$config['turnitin_owner'] = 'UUID';
+
+// Submitter used to submit submissions
+$config['turnitin_submitter'] = 'UUID';
+
+// User used to view the similarities
+$config['turnitin_viewer'] = 'UUID';
+
